95e67b037b69d948689cf97a5df1bc3c6ebd9264
Aremoh Kenneth <kenneth.aremoh@gmail.com>
2020-03-04 19:23:19 +0100

    corrected white noise

--- bgn/settings.py
+++ bgn/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'Whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
